3af1617419ae2da313b44ad57fb83ef7ce4809d0
jjmmnd <rnastery1@gmail.com>
2025-07-21 16:13:09 +0900

    Initial Commit

---
+++ src/main/java/hongik21/fit_a_pet/FitAPetApplication.java
@@ -0,0 +1,13 @@
+package hongik21.fit_a_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitAPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitAPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongik21/fit_a_pet/FitAPetApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik21.fit_a_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitAPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
