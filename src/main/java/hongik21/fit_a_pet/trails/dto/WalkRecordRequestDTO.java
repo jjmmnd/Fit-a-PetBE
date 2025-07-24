ae040169ffee54c28407604f39de9c2a21b61a5d
umhye1 <lucy021206@gmail.com>
2025-07-24 20:51:00 +0900

    산책 기록 저장, 월별 기록 조회, 날짜별 기록 조회 service

---
+++ src/main/java/hongik21/fit_a_pet/trails/dto/WalkRecordRequestDTO.java
@@ -0,0 +1,24 @@
+package hongik21.fit_a_pet.trails.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// 프론트 -> 백엔드로 산책 기록 저장 요청을 보낼 때 사용
+// 요청 DTO는 Spring이 setter로 자동 채워주기 때문에 생성자 불필요,
+@Getter
+@Setter
+public class WalkRecordRequestDTO {
+
+    // 문자열로 받아서 LocalTime, LocalDate로 변경하는게 일반적이라고 함
+    private String walk_date;
+    private String walk_start;
+    private String walk_end;
+
+    private Float distance;
+    private Integer rating;
+    private String memo;
+
+    private Integer pet_id;
+    private Integer user_id;
+
+}
