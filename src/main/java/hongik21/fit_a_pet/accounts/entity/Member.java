42c1d7cc6cc087a965be21b2df0283f025503fab
jjmmnd <rnastery1@gmail.com>
2025-07-28 02:37:54 +0900

    [Chore] Member entity, repo, enum 파일 생성

---
+++ src/main/java/hongik21/fit_a_pet/accounts/entity/Member.java
@@ -0,0 +1,39 @@
+package hongik21.fit_a_pet.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "profile_url", columnDefinition = "text")
+    private String profileUrl;
+
+    private MemberType memberType;
+    private String password;
+    private String nickname;
+    private String residence;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime deletedAt;
+}
--- src/main/java/hongik21/fit_a_pet/accounts/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hongik21.fit_a_pet.accounts.repository;
-
-import hongik21.fit_a_pet.accounts.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
