5d3679e4cf2866345194b337460ff4081dfd3a3d
jjmmnd <rnastery1@gmail.com>
2025-07-29 22:11:09 +0900

    [Feat] 인증 만료 코드 자동 삭제 로직 구현

--- src/main/java/hongik21/fit_a_pet/accounts/repository/VerificationCodeRepository.java
+++ src/main/java/hongik21/fit_a_pet/accounts/repository/VerificationCodeRepository.java
@@ -4,9 +4,11 @@ import hongik21.fit_a_pet.accounts.entity.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
     Optional<VerificationCode> findByEmailAndCode(String email, String code);
+    void deletedByExpiryTimeBefore(LocalDateTime now);
 }
