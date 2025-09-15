75fec02603df3d91e26377fc4af20f1ffd76edfb
jjmmnd <rnastery1@gmail.com>
2025-09-15 17:06:56 +0900

    [chore] Comment 엔티티 정의

---
+++ src/main/java/hongik21/fit_a_pet/comments/entity/Comment.java
@@ -0,0 +1,34 @@
+package hongik21.fit_a_pet.comments.entity;
+
+import hongik21.fit_a_pet.accounts.entity.Member;
+import hongik21.fit_a_pet.posts.domain.Post;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
